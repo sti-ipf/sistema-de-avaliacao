a17415e4b99c7ef660f78b5c5d92d5cda5f93a1a
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2012-10-30 20:17:38 -0200

    adicionado item questoes no menu

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -1,40 +1,40 @@
-ActiveAdmin.register_page "Questions" do
-      menu :label => "Questões", :parent => "Avaliação"
-#   menu :parent => "Avaliação", :priority => 7
+# ActiveAdmin.register_page "Questions" do
+#       menu :label => "Questões", :parent => "Avaliação"
+# #   menu :parent => "Avaliação", :priority => 7
 
-#   # controller do 
-#   #   def index
-#   #     @questions = Question.all(:include => [:question_texts])
-#   #   end
-#   # end
+# #   # controller do 
+# #   #   def index
+# #   #     @questions = Question.all(:include => [:question_texts])
+# #   #   end
+# #   # end
 
-#   # index do |q|
-#   #   column "Indicador - Número" do |question|
-#   #     "#{question.indicator.number}.#{question.number}"
-#   #   end
-#   #   column "Texto" do |question|
-#   #     question.question_texts.first.text
-#   #   end
-#   #   default_actions
-#   # end
+# #   # index do |q|
+# #   #   column "Indicador - Número" do |question|
+# #   #     "#{question.indicator.number}.#{question.number}"
+# #   #   end
+# #   #   column "Texto" do |question|
+# #   #     question.question_texts.first.text
+# #   #   end
+# #   #   default_actions
+# #   # end
 
-#   # action_item do
-#   #   link_to "Importar", import_instrument_path
-#   # end
+# #   # action_item do
+# #   #   link_to "Importar", import_instrument_path
+# #   # end
 
-#   # form :partial => "form"
+# #   # form :partial => "form"
 
-#   # show do |s|
-#   #   attributes_table do
-#   #     row :id
-#   #     row :number
-#   #     row :service_level
-#   #     row :indicator
-#   #     row :question_texts do
-#   #       (s.question_texts.map{ |p| p.text }).join(', ')
-#   #     end
-#   #   end
-#   # end
+# #   # show do |s|
+# #   #   attributes_table do
+# #   #     row :id
+# #   #     row :number
+# #   #     row :service_level
+# #   #     row :indicator
+# #   #     row :question_texts do
+# #   #       (s.question_texts.map{ |p| p.text }).join(', ')
+# #   #     end
+# #   #   end
+# #   # end
 
-end
+# end
 
